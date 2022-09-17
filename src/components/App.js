2dfa18c38b8a2ff38585c2ca43baa75a5feb4029
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2022-09-17 12:12:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from '../AppRoutes';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "../AppRoutes";
 
-import '../styles/App.css';
+import "../styles/App.css";
 const App = () => {
-
   return (
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-
-  )
-}
-
+  );
+};
 
 export default App;
