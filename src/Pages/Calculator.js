b6b623859ecebd9a60e6be510a85679c998f27d3
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2022-09-17 12:05:59 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -2,8 +2,40 @@ import React from "react";
 
 
 export const Calculator = () =>{
-
+     let url_string = window.location.href;
+    let url = new URL(url_string);
+    let num1 = Number(url.searchParams.get("num1");
+    let num2 = Number(url.searchParams.get("num2");
+    let op = url.searchParams.get("op");
+   console.log(num1, num2, op);
+    let ans = 0;
+    
+    switch(op){
+        case "+":{
+            ans = num1+num2;
+            break;
+        }
+            case "/":{
+            ans = num1/num2;
+            break;
+        }
+            case "-":{
+            ans = num1-num2;
+            break;
+        }
+            case "*":{
+            ans = num1*num2;
+            break;
+        }
+             case "%":{
+            ans = num1%num2;
+            break;
+        }
+            
+    }
     return(
-        
+        <div>
+            Your calculation Result is <span id="calc-result">{ans}</span>
+        </div>
     )
 }
