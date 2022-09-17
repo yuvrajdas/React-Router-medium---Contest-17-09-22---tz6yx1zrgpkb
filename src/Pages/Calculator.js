20e4c71f69bf8ad17a608cb2e75aace6c1e6e87b
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2022-09-17 12:11:11 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,41 +1,40 @@
 import React from "react";
 
-
-export const Calculator = () =>{
-     let url_string = window.location.href;
-    let url = new URL(url_string);
-    let num1 = Number(url.searchParams.get("num1");
-    let num2 = Number(url.searchParams.get("num2");
-    let op = url.searchParams.get("op");
-   console.log(num1, num2, op);
-    let ans = 0;
-    
-    switch(op){
-        case "+":{
-            ans = num1+num2;
-            break;
-        }
-            case "/":{
-            ans = num1/num2;
-            break;
-        }
-            case "-":{
-            ans = num1-num2;
-            break;
-        }
-            case "*":{
-            ans = num1*num2;
-            break;
-        }
-             case "%":{
-            ans = num1%num2;
-            break;
-        }
-            
+export const Calculator = () => {
+  let url_string = window.location.href;
+  var url = new URL(url_string);
+  var num1 = Number(url.searchParams.get("num1"));
+  var num2 = Number(url.searchParams.get("num2"));
+  var op = url.searchParams.get("op");
+  console.log(num1, num2, op);
+  let ans = 0;
+  console.log(op);
+  switch (op) {
+    case "+": {
+      ans = num1 + num2;
+      break;
     }
-    return(
-        <div>
-            Your calculation Result is <span id="calc-result">{ans}</span>
-        </div>
-    )
-}
+    case "/": {
+      ans = num1 / num2;
+      break;
+    }
+    case "-": {
+      ans = num1 - num2;
+      break;
+    }
+    case "*": {
+      ans = num1 * num2;
+      break;
+    }
+    case "%": {
+      ans = num1 % num2;
+      break;
+    }
+  }
+
+  return (
+    <div>
+      Your calculation Result is <span id="calc-result">{ans}</span>
+    </div>
+  );
+};
